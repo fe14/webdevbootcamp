8a28060ad789c6713127a31b86f2becb77d7c64e
Fernanda Palmieri <fernanda.palmieri@gmx.at>
2019-11-27 10:27:32 +0100

    for loops

---
+++ for.js
@@ -0,0 +1,26 @@
+console.log ("Numbers -10 to 19")
+for(var x = -10; x <20; x++){
+    console.log(x);
+    
+}
+    
+console.log ("Numbers 10 to 40")
+for(var x = 10; x <=40; x++){
+    if(x % 2 === 0){
+        console.log(x);
+    }
+}
+
+console.log ("Numbers 300 to 333")
+for(var x = 300; x <=333; x++){
+    if(x % 2 === 1){
+        console.log(x);
+    }
+}
+
+console.log ("Numbers 5 to 50")
+for(var x = 5; x <=50; x++){
+        if(x % 5 === 0 && x % 3 === 0){
+            console.log(x);
+    }
+}
